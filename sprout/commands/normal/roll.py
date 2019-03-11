89afaf7bff9f36f211370e1e91326e9d77593851
Fycho <fycho127@gmail.com>
2019-03-11 16:40:06 +0800

    add 概率判断

--- sprout/commands/normal/roll.py
+++ sprout/commands/normal/roll.py
@@ -7,5 +7,4 @@ async def run(bot, ctx, cmd, arg) -> None:
     if ctx['message'].endswith('概率'):
         point += '%'
 
-    point = random.randint(0, 100)
     await bot.send(ctx, message=f'你摇到了点数：{point}', at_sender=True)
