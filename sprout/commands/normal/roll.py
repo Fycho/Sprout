be431cb41b679e75d96d8d8b3e32569f97102247
Fycho <fycho127@gmail.com>
2019-03-11 16:25:18 +0800

    fix typo

--- sprout/commands/normal/roll.py
+++ sprout/commands/normal/roll.py
@@ -1,6 +1,6 @@
 import random
 
+
 async def run(bot, ctx, cmd, arg) -> None:
     point = random.randint(0, 100)
-    await bot.send(ctx, message=f'你摇到了点数：{point}',at_sender=True)
-
+    await bot.send(ctx, message=f'你摇到了点数：{point}', at_sender=True)
