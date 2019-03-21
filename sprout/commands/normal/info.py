d8adeb9bc071c3e9a86905451f458c6c9d496ba2
Fycho <fycho127@gmail.com>
2019-03-21 15:30:58 +0800

    add info

--- sprout/commands/normal/info.py
+++ sprout/commands/normal/info.py
@@ -1,5 +1,3 @@
 async def run(bot, ctx, cmd, arg) -> None:
-    message = '''
-这里是豆芽菜，也可以叫我DDBot，如有任何问题请联系主人Fycho。
-    '''
+    message = '这里是豆芽菜，也可以叫我DDBot，使用过程入遇任何问题请联系主人飞球。'
     await bot.send(ctx, message=message)
