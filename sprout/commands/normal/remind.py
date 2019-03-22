c51c20ebd6ddfe345037abd242c247793217d2d2
Fycho <fycho127@gmail.com>
2019-03-22 12:59:47 +0800

    add remind

--- sprout/commands/normal/remind.py
+++ sprout/commands/normal/remind.py
@@ -8,23 +8,23 @@ async def run(bot, ctx, cmd, arg) -> None:
     if not arg:
         return await bot.send(ctx, message='缺少参数')
 
-    args = re.split('\s+', arg)
-
-    if (len(args) < 2):
+    matched = re.match(r'^(\w+)\s+(.*?)$', arg)
+    if matched:
+        time = matched.group(1)
+        msg = matched.group(2)
+    else:
         return await bot.send(ctx, message='缺少参数')
 
-    second = args[0]
-    msg = args[1]
-
-    if is_number(second) == False:
+    if is_number(time) == False:
         return await bot.send(ctx, message='参数#1只能为数字')
-    second = int(second)
 
-    delayed_task = asyncio.create_task(delay(bot, ctx, second, msg))
-    reply_task = bot.send(ctx, message=f'收到，会在{second}秒后提醒你：{msg}', at_sender=True)
+    time = int(time)
+
+    delayed_task = asyncio.create_task(delay(bot, ctx, time, msg))
+    reply_task = bot.send(ctx, message=f'收到，会在{time}秒后提醒你：{msg}', at_sender=True)
     asyncio.gather(delayed_task, reply_task)
 
 
-async def delay(bot, ctx, second, msg):
-    await asyncio.sleep(second)
+async def delay(bot, ctx, time, msg):
+    await asyncio.sleep(time)
     await bot.send(ctx, message=msg, at_sender=True)
