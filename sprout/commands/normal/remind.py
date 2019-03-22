f4145a833ce7f275edf95554255c0e41395530b1
Fycho <fycho127@gmail.com>
2019-03-22 13:27:00 +0800

    add remind

--- sprout/commands/normal/remind.py
+++ sprout/commands/normal/remind.py
@@ -8,19 +8,17 @@ async def run(bot, ctx, cmd, arg) -> None:
     if not arg:
         return await bot.send(ctx, message='缺少参数')
 
-    matched = re.match(r'^(.*)\s+(.*?)$', arg)
+    matched = re.match(r'^(\w+)\s+(.*?)$', arg)
     if matched:
         time = matched.group(1)
         msg = matched.group(2)
     else:
-        return await bot.send(ctx, message='缺少参数')
+        return await bot.send(ctx, message='缺少参数或者参数不合法')
 
     if is_number(time) == False:
         return await bot.send(ctx, message='参数#1只能为数字')
 
     time = int(time)
-    if time < 0:
-        return await bot.send(ctx, message='参数#1不能小于0')
 
     delayed_task = asyncio.create_task(delay(bot, ctx, time, msg))
     reply_task = bot.send(ctx, message=f'收到，会在{time}秒后提醒你：{msg}', at_sender=True)
