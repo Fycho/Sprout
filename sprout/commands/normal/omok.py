e59cb6c3167a9db12b2e38c9ee5f7c7ac5485b2d
Fycho <fycho127@gmail.com>
2019-04-05 00:04:41 +0800

    add omok

--- sprout/commands/normal/omok.py
+++ sprout/commands/normal/omok.py
@@ -5,7 +5,7 @@ from sprout import Sprout
 
 async def run(bot: Sprout, ctx, cmd, arg) -> None:
     if not arg or arg == 'help':
-        message = '双人对战五子棋，确认加入/omok join，退出游戏/omok quit。一方加入后30秒内无另一方加入则自动退出。游戏开始后如一方30秒无输入则游戏自动结束。'
+        message = '【请在电脑上使用！！勿在手机上使用！！】双人对战五子棋，确认加入/omok join，退出游戏/omok quit。一方加入后30秒内无另一方加入则自动退出。游戏开始后如一方30秒无输入则游戏自动结束。'
         await bot.send(ctx, message=message)
 
     if arg == 'join':
