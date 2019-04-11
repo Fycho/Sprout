16dbc71140e6eb5366a4d27f6a4e2a54e9a92afa
Fycho <fycho127@gmail.com>
2019-04-12 00:29:44 +0800

    decrease probability of turing reply

--- sprout/commands/admin/sql.py
+++ sprout/commands/admin/sql.py
@@ -3,13 +3,13 @@ import sqlite3
 
 
 async def run(bot, ctx, cmd, arg) -> None:
-    try:
-        with sqlite3.connect(bot.config.db) as connect:
+    with sqlite3.connect(bot.config.db) as connect:
+        try:
             c = connect.cursor()
             c.execute(arg)
             r = c.fetchall()
             message = json.dumps(r, ensure_ascii=False)
-    except sqlite3.Error as err:
-        message = json.dumps(err, ensure_ascii=False)
+        except sqlite3.Error as err:
+            message = json.dumps(err, ensure_ascii=False)
 
     await bot.send(ctx, message)
