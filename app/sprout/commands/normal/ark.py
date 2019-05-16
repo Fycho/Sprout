7c9da80359dcc56d02b14bddc78ef825ac79a75e
Fycho <fycho127@gmail.com>
2019-05-16 16:54:15 +0800

    add ark

--- app/sprout/commands/normal/ark.py
+++ app/sprout/commands/normal/ark.py
@@ -12,10 +12,10 @@ def get_employee_data():
 async def handle_index(bot, ctx):
     message = '''/ark 明日方舟指令帮助：
 /ark info [名字] - 查看干员信息
-/ark draw - 模拟私有寻访（无up、保底计算）
-/ark crazy - 模拟十连（无up、保底计算）'''
+/ark draw - 模拟一次干员寻访（无up、保底计算）
+/ark crazy - 模拟十连干员寻访（无up、保底计算）'''
 # / ark calc[标签1 | 标签2..] - 公开招募计算器「未开放」
-# /ark recruit [标签1|标签2..] - 模拟公开招募「未开放」'''
+# /ark recruit [标签1|标签2..] - 模拟公开招募「未开放」
     return await bot.send(ctx, message=message, at_sender=True)
 
 
@@ -50,7 +50,7 @@ async def handle_ten_draw(bot, ctx):
 
 async def run(bot, ctx, cmd, arg) -> None:
     if not arg:
-        return await handle_index(    bot, ctx)
+        return await handle_index(bot, ctx)
 
     args = re.split('\s+', arg)
     sub_cmd = args[0]
