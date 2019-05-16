0a7fcd4ee5b12c7c7159182d03f3688431f8946a
Fycho <fycho127@gmail.com>
2019-05-16 12:27:40 +0800

    add ark

--- app/sprout/config.py
+++ app/sprout/config.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 DEBUG = False
 SUPER_USERS = {os.environ.get('SUPER_ACCOUNT')}
 ADMIN_COMMANDS = {'ping', 'task'}
-NORMAL_COMMANDS = {'info', 'weather', 'vtb', 'roll', 'moegirl', 'remind', 'help', 'rps', 'omok', 'tzfe', 'word'}
+NORMAL_COMMANDS = {'info', 'weather', 'vtb', 'roll', 'moegirl', 'remind', 'help', 'rps', 'omok', 'tzfe', 'word', 'ark'}
 
 APSCHEDULER_CONFIG: Dict[str, Any] = {
     'apscheduler.timezone': 'Asia/Shanghai'
